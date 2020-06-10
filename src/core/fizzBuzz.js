264177e9d14d19a7b4e1eb1e859d93649a3031e5
Jack <jack.parra253@gmail.com>
2020-06-10 14:11:47 -0500

    refactoring fizzbuzz with pattern matching

--- src/core/fizzBuzz.js
+++ src/core/fizzBuzz.js
@@ -1,16 +1,13 @@
+const { match } = require("x-match-expression");
+
 function fizzBuzz(number) {
-    const divibleBy = (divider, number) => number % divider === 0;
+    const divibleBy = divider => number => number % divider === 0;
 
-    if(divibleBy(15, number))
-        return 'fizzbuzz';
-
-    if(divibleBy(3, number))
-        return 'fizz';
-
-    if(divibleBy(5, number))
-        return 'buzz';
-
-    return number;
+    return match(number)
+        .case(divibleBy(15), 'fizzbuzz')
+        .case(divibleBy(3), 'fizz')
+        .case(divibleBy(5), 'buzz')
+        .default(number);
 }
 
 module.exports = fizzBuzz;
